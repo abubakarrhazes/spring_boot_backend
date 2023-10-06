1d3c180448f9de35ada3a26595e0aa998b0dd56d
alrhazes <anuuman2@gmail.com>
2023-10-06 17:15:35 +0100

    commit

--- src/main/java/com/example/spring_boot_backend/SpringBootBackendApplication.java
+++ src/main/java/com/example/spring_boot_backend/SpringBootBackendApplication.java
@@ -2,12 +2,23 @@ package com.example.spring_boot_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootBackendApplication.class, args);
+
+	}
+
+
+	@GetMapping
+	public String GetPage(){
+
+		return "Get Page Working Fine";
 	}
 
 }
